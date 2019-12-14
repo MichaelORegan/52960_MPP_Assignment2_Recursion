bc3461f8e431f1797f9bec1e1e6707aec42da046
MichaelORegan <g00364738@gmit.ie>
2019-12-14 20:31:31 +0000

    q10 added

--- 10_verify_parentheses_recursion.py
+++ 10_verify_parentheses_recursion.py
@@ -1,6 +1,38 @@
-# https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-python/
 # https://stackoverflow.com/questions/18007995/recursive-method-for-parentheses-balancing-python
 
-def verify_brackets(n):
-    if len(n) == 0:
-        return True
+# define the function, string n with (), 
+# i in n start at 0, counter start at 0
+def verify_parentheses(n, i=0, counter=0):
+    # if there is nothing in the string 
+    if i == len(n):
+    # return 0 balanced True
+        return counter == 0
+    #if the counter is less than 0
+    if counter < 0:
+    # it is not balanced and return False
+        return False
+    # starting at positon 0 as per i's value, if this is "("
+    if n[i] == "(":
+    # call the function on the next position, increase i and increase the counter
+        return  verify_parentheses(n, i + 1, counter + 1)
+    # or if the positon 0 as per i's value, if this is ")"
+    elif n[i] == ")":
+    # call the function on the next position, increase i and reduce the counter
+        return  verify_parentheses(n, i + 1, counter - 1)
+    # return the boolean answer whether the string is balanced or not
+    return verify_parentheses(n, i + 1, counter)
+
+a = "(()()"
+print(verify_parentheses(a))
+b = "(()"
+print(verify_parentheses(b))
+c = "(())"
+print(verify_parentheses(c))
+d = ")(()"
+print(verify_parentheses(d))
+e = "())(())("
+print(verify_parentheses(e))
+f = "()((("
+print(verify_parentheses(f))
+g = ""
+print(verify_parentheses(g))
